7f5c5f6e01453aad7b61de39a6f52d13c51e8d35
Joe Lloyd <josephglloyd@gmail.com>
2015-07-19 18:07:43 +0100

    Added basic console app

---
+++ StoppitCLI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoppitCLI
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Do as you're told!");
+
+			Console.ReadLine();
+		}
+	}
+}
